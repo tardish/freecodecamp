1c82162086a23e030b003aed34cbd78b75ab6acc
Panudet Namna <panudet.mt@gmail.com>
2021-02-24 16:52:03 +0700

    fix

--- myApp.js
+++ myApp.js
@@ -1,40 +1,5 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-console.log('Hello World')
+console.log("Hello World");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
